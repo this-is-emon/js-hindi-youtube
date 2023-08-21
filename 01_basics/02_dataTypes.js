3eaa6b14a4903732a6c61026af4c0bdd23d32854
Mofijul Islam Emon <53686083+this-is-emon@users.noreply.github.com>
2023-08-21 07:29:00 +0000

    data types done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // We are using node.js, not browser
+
+let name = "Emon"
+let age = 18
+let isLoggedIn = false
+
+/* Primitive Data Types */
+// number => 2^35
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined
+// symbol => unique 
+
+/* Non Primitive Data Types */
+// object
+// string
+
+console.log(typeof name); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
