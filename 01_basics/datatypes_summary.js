9afabbff0c940cd2c8aec39f3beb8a9bb5a926aa
Mofijul Islam Emon <53686083+this-is-emon@users.noreply.github.com>
2023-08-23 08:59:54 +0000

    stack and heap memory explained

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId); // False
+//console.log(id === anotherId); // False
 
 // const bigNumber = 3456543576654356754n
 
@@ -32,7 +32,7 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
@@ -42,3 +42,30 @@ console.log(typeof anotherId);
 /*
     JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
 */
+
+/***************  Memory: Heap vs Stack  *****************/
+// See the "heap_stack.png" file to understand the code below
+
+//Stack(Primitive Type), 
+let myYoutubeName = "Hitesh Chawdhury"
+
+let anotherName = myYoutubeName
+anotherName = "Chai aur Code"
+
+// Stack memory send a copy of values from one variable-memory to another
+console.log(myYoutubeName) //Hitesh Chawdhury
+console.log(anotherName) // Chai aur Code
+
+//Heap(Non-Primitive Type)
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+// Heap memory refer value of an variable-memory to another
+console.log(userOne) // { email: 'hitesh@google.com', upi: 'user@ybl' }
+console.log(userTwo) // { email: 'hitesh@google.com', upi: 'user@ybl' }
