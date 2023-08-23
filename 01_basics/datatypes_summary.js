e965c18f23374edae42f6a82309886a4c8adb902
Mofijul Islam Emon <53686083+this-is-emon@users.noreply.github.com>
2023-08-23 03:30:50 +0000

    Data type summary for interview Q&A

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,44 @@
+//Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); // False
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+/***************  Q&A  *****************/
+
+// does js is statically typed or dynamically typed ?
+/*
+    JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+*/
