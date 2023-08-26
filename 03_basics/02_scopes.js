5ab3fdb6f9508f177930114a1d0738c92f551ebd
Mofijul Islam Emon <53686083+this-is-emon@users.noreply.github.com>
2023-08-26 16:52:36 +0000

    Nested scope and mini Hoisting explained

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,4 +1,5 @@
 // Link : https://www.youtube.com/watch?v=cHHU0jXfjKY&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=21
+// Part 01 : Global and Local scope Introduction
 
 /* No problem Eexecuting the following code */
 // let a = 10
@@ -29,15 +30,15 @@
 /* -------------------------------------------------------- */
 
 /* Watch out the following code */
-let a = 300
-if (true) {
-    let a = 10
-    const b = 20
-    console.log("INNER: ", a);  //INNER:  10 => block scope
+// let a = 300
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     console.log("INNER: ", a);  //INNER:  10 => block scope
     
-}
+// }
 
-console.log(a); //300 => global scope
+// console.log(a); //300 => global scope
 
 /* -------------------------------------------------------- */
 
@@ -49,5 +50,57 @@ console.log(a); //300 => global scope
 /* -------------------------------------------------------- */
 
 
+// Link : https://www.youtube.com/watch?v=eWwge2YpHhc&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=22
+// Part 02 : Scope level and mini hoisting in javascript
 
+/***** As a child 'two()' can access 'one()' variable, but vice-versa doesn't happens *****/
+// This is a kind of closure example
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website); //ReferenceError: website is not defined
+
+    two()
+
+}
+one() //hitesh
+
+/* -------------------------------------------------------- */
+
+/***** Another example of the difference between Global and Local scopes     *****/
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        console.log(username + website); //hitesh youtube
+    }
+    // console.log(website); ////ReferenceError: website is not defined
+}
+
+// console.log(username); ////ReferenceError: username is not defined
+/* -------------------------------------------------------- */
+
+
+/***** Interesing Example (Concept : Hoisting) *****/
+
+console.log(addone(5)) //5
+
+function addone(num){ //basic funcion
+    return num + 1
+}
+
+
+
+addTwo(5) //ReferenceError: Cannot access 'addTwo' before initialization
+const addTwo = function(num){ // Expression (a kind of function that is assigned to a variable)
+    return num + 2
+}
+
+// Learning: 
+// => Cannot call a expression before declaring it 
+/* -------------------------------------------------------- */
 
