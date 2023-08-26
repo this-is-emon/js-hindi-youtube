a8256298093d599fe372389fdeaae9d2bb0eab56
Mofijul Islam Emon <53686083+this-is-emon@users.noreply.github.com>
2023-08-26 07:42:37 +0000

    destructring, Json explained

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,4 +1,5 @@
-// Link:  https://www.youtube.com/watch?v=4lb2pXWWXJI&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=17
+// Link1:  https://www.youtube.com/watch?v=4lb2pXWWXJI&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=17
+// Link2:  https://www.youtube.com/watch?v=AViTh83k-IE&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=18
 
 /* Singleton Object */
 //const tinderUser = Object()
@@ -52,12 +53,45 @@ const users = [
     }  
 ]
 
-console.log(users[1].email);  //i@gmail.com
-console.log(tinderUser) //{ id: '123abc', name: 'Sammy', isLoggedIN: false }
-console.log(Object.keys(tinderUser)); //[ 'id', 'name', 'isLoggedIN' ] => *data Type: Array
-console.log(Object.values(tinderUser)); //[ '123abc', 'Sammy', false ] => *data Type: Array
-console.log(Object.entries(tinderUser)); //[ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIN', false ] ]
+// console.log(users[1].email);  //i@gmail.com
+// console.log(tinderUser) //{ id: '123abc', name: 'Sammy', isLoggedIN: false }
+// console.log(Object.keys(tinderUser)); //[ 'id', 'name', 'isLoggedIN' ] => *data Type: Array
+// console.log(Object.values(tinderUser)); //[ '123abc', 'Sammy', false ] => *data Type: Array
+// console.log(Object.entries(tinderUser)); //[ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIN', false ] ]
 
 
-// Looping through an object and check, if an property exists?
-console.log(tinderUser.hasOwnProperty("isLoggedIn")); //false
+// // Looping through an object and check, if an property exists?
+// console.log(tinderUser.hasOwnProperty("isLoggedIn")); //false
+
+/*************** Object de-structure and JSON API intro *****************/
+
+const course ={
+    courseName: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+//console.log(course.courseInstructor); //hitesh
+
+/*--De-structuring--*/
+
+// const {courseInstructor} = course
+// console.log(courseInstructor); //hitesh
+
+const {courseInstructor:instructor} = course
+console.log(instructor); //hitesh
+
+/***Incoming data from an api in JSON format (here, object type json)***/
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+/***Incoming data from an api in JSON format (here, Arry object type json)***/
+// [
+//     {},
+//     {},
+//     {}
+// ]
