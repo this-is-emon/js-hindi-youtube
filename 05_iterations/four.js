eb078ac079d2819957be2fe93c146d0fd8515caa
Mofijul Islam Emon <53686083+this-is-emon@users.noreply.github.com>
2023-08-30 06:21:33 +0000

    for-in, object vs Map

---
+++ 05_iterations/four.js
@@ -0,0 +1,22 @@
+//Iterating through on object using 'for-in'
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+// f0r-in can not be used to iterate through map
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
