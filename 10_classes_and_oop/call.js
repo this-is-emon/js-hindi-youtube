1bf79003b91bbb64e87fac0a2aa398c7c5971b0b
this-is-emon <mofijul.islam.emon@gmail.com>
2023-09-27 17:21:39 +0600

    Call and this video done

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,15 @@
+function SetUsername(SetUsername) {
+    //complex DB calls
+    this.username = this.username
+    console.log("called");
+}
+
+function createUser(username,email,password) {
+    SetUsername.call(this,username)
+
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai","chai@fb.com","123")
+console.log(chai);
