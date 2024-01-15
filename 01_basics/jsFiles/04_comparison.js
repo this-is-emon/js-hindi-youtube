4b7ec1d5a6cc4a87e5051a6301776ed9a1db7b6b
this-is-emon <mofijul.islam.emon@gmail.com>
2024-01-15 20:41:12 +0600

    comparison in Ts done

--- 01_basics/jsFiles/04_comparison.js
+++ 01_basics/jsFiles/04_comparison.js
@@ -2,24 +2,25 @@
 // Topic : Comparison of datatypes in javascript
 
 /******** Common Type of comparison **********/
-// console.log(2 > 1)
-// console.log(2 >= 1)
-// console.log(2 < 1)
-// console.log(2 <= 1)
-// console.log(2 == 1)
-// console.log(2 != 1)
+console.log(2 > 1) //true
+console.log(2 >= 1) //true
+console.log(2 < 1) //false
+console.log(2 <= 1) //false
+console.log(2 == 1) //false
+console.log(2 != 1) //true
+console.log("---01-----") 
 
 /******** Comparison that should be avoided **********/
 console.log("2" > 1) //true
 console.log("02" > 1) //true
 
-console.log("--------") 
+console.log("---02-----") 
 
 console.log(null > 0) //false
 console.log(null == 0) //false
 console.log(null >= 0) //true
 
-console.log("--------")
+console.log("---03-----")
 
 console.log(undefined == 0) //false
 console.log(undefined > 0) //false
