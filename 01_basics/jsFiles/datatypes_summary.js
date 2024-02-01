1249a9ab88ad0d7738dbf1dbd37844446197597e
this-is-emon <mofijul.islam.emon@gmail.com>
2024-02-01 23:08:56 +0600

    data types updated

--- 01_basics/jsFiles/datatypes_summary.js
+++ 01_basics/jsFiles/datatypes_summary.js
@@ -43,7 +43,10 @@ const myFunction = function(){
 
 // does js is statically typed or dynamically typed ?
 /*
-    JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+    JavaScript is a dynamically typed language, which means that 
+    data types of variables are determined by the value they hold
+    at runtime and can change throughout the program as we assign
+    different values to them.
 */
 
 
---
+++ 01_basics/tsFiles/datatypes_summaryInTs.ts
@@ -0,0 +1,16 @@
+// does Ts is statically typed or dynamically typed ?
+/*
+TypeScript is statically typed. 
+
+Static typing means that variable types are known and checked at compile-time rather than runtime. 
+In TypeScript, you declare the type of a variable explicitly, and the TypeScript compiler enforces 
+that the types are used correctly. This helps catch type-related errors early in the development 
+process, providing better code quality and reducing runtime errors.
+
+Contrastingly, dynamically typed languages, like JavaScript, determine types at runtime. 
+Variables in dynamically typed languages can hold values of any type, and type checking occurs 
+during program execution.
+
+By introducing static typing, TypeScript enhances the development experience by offering 
+better tooling support, code completion, and catching potential errors before running the code.
+*/
