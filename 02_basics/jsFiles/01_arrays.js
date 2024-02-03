bf6ff415f86a736d6caa947907f3c9a46f0ec710
this-is-emon <mofijul.islam.emon@gmail.com>
2024-02-03 16:34:16 +0600

    minor changes

--- 02_basics/jsFiles/01_arrays.js
+++ 02_basics/jsFiles/01_arrays.js
@@ -3,14 +3,14 @@
 
 /***** Array *****/
 // "array literal notation"
-const myArr = [0,1,2,3,4,5,]
+const myArr = [0,1,2,3,4,5]
 const myHeros = ["shaktiman","maagraj"]
 //"array creation using the Array constructor"
 const myArr2 = new Array(1,2,3,4)
 
 //console.log(myArr[0])
 
-// Array methods
+/* -----Array methods----- */
 
 // myArr.push(6)
 // myArr.push(7)
@@ -25,23 +25,23 @@ const myArr2 = new Array(1,2,3,4)
 // console.log(myArr.includes(9)); //false
 // console.log(myArr.indexOf(3)); // 3
 
-// const newArr = myArr.join() // "0, 1, 2, 4, 5"
+const newArr = myArr.join() // "0, 1, 2, 4, 5"
 
-// console.log(myArr); //[ 0, 1, 2, 4, 5 ]
-// console.log(typeof myArr); //object
-// console.log(newArr); // 0,1,2,4,5
-// console.log(typeof newArr); //string
+// console.log(myArr); // [ 0, 1, 2, 4, 5 ]
+// console.log(typeof myArr); // object
+// console.log(newArr); //  0,1,2,4,5
+// console.log(typeof newArr); // string
 
 
-// slice, splice (Difference?)
-console.log("A ",myArr); // A  [ 0, 1, 2, 3, 4, 5 ]
+/* -----slice, splice (Difference?)----- */
+//console.log("A ",myArr); // A  [ 0, 1, 2, 3, 4, 5 ]
 
 const myn1 = myArr.slice(1,3) //[ 1, 2 ] 
-console.log(myn1); //
-console.log("B ",myArr); // B  [ 0, 1, 2, 3, 4, 5 ]
+//console.log(myn1); //
+//console.log("B ",myArr); // B  [ 0, 1, 2, 3, 4, 5 ]
 
 const myn2 = myArr.splice(1,3)
-console.log("C ",myArr); //C  [ 0, 4, 5 ]
-console.log(myn2) // [ 1, 2, 3 ]
+//console.log("C ",myArr); //C  [ 0, 4, 5 ]
+//console.log(myn2) // [ 1, 2, 3 ]
 
 
