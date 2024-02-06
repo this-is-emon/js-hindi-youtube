016f447eedb7ef2df995b0fd6074d897675ce6b3
this-is-emon <mofijul.islam.emon@gmail.com>
2024-02-06 13:41:25 +0600

    Update both in TS and JS file

--- 02_basics/jsFiles/04_objects.js
+++ 02_basics/jsFiles/04_objects.js
@@ -1,12 +1,18 @@
 // Link1:  https://www.youtube.com/watch?v=4lb2pXWWXJI&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=17
-// Link2:  https://www.youtube.com/watch?v=AViTh83k-IE&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=18
+/* KeyTopics : constructor object; Object lateral; nested object; 
+joining object using- `{obj,obj}`,Object.assign({},obj1,obj2),{...obj1,...obj2}; */
 
-/* Singleton Object */
+// Link2:  https://www.youtube.com/watch?v=AViTh83k-IE&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=18
+/* Key Topics : Objects.keys(); Object.values(); Object.entries();objectName.hasWonProperty("propertyName");
+   Destructuring, JSON Type: 'Object type', 'Array object Type' */
+
+
+
+/* Singleton Object `OR` Declaring object using Constructor */
 //const tinderUser = Object()
 
-/* Non-Singleton Object */
+/* Non-Singleton Object `OR` Object Lateral  */
 const tinderUser = {}
-
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIN = false
@@ -38,6 +44,7 @@ const obj2 = {3:"c",4:"d"}
 // const obj3 = {...obj1,...obj2}
 // console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
 
+
 const users = [
     {
         id:1,
@@ -60,8 +67,10 @@ const users = [
 // console.log(Object.entries(tinderUser)); //[ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIN', false ] ]
 
 
-// // Looping through an object and check, if an property exists?
+// Looping through an object and check, if an property exists?
 // console.log(tinderUser.hasOwnProperty("isLoggedIn")); //false
+
+/* ------------------------------Link 01 code done------------------------------ */
 
 /*************** Object de-structure and JSON API intro *****************/
 
@@ -73,16 +82,34 @@ const course ={
 
 //console.log(course.courseInstructor); //hitesh
 
-/*--De-structuring--*/
+/*--De-structuring Object--*/
 
+// Way : 01
 // const {courseInstructor} = course
 // console.log(courseInstructor); //hitesh
 
+// Way : 02
 const {courseInstructor:instructor} = course
-console.log(instructor); //hitesh
+//console.log(instructor); //hitesh
+
+/*--De-structuring Array--*/
+let myArray = [1, 2, 3];
+
+let [firstValue, secondValue, thirdValue] = myArray;
+
+// console.log(firstValue);  // Output: 1
+// console.log(secondValue); // Output: 2
+// console.log(thirdValue);  // Output: 3
+
+/* ****** What is an API? ****** */
+//APIs enable different pieces of software to work together, 
+//allowing them to share and use each other's functionalities or data.
+
+/* -----There are 2 difference between 'Object and 'JSON Api'----- */ 
+// 1. JSON data does not have a 'name' like object(ex: cons myTask = {.....})
+// 2. 'Key' of the JSON data always wrapped by double quote("")
 
 /***Incoming data from an api in JSON format (here, object type json)***/
-
 // {
 //     "name": "hitesh",
 //     "coursename": "js in hindi",
@@ -95,3 +122,8 @@ console.log(instructor); //hitesh
 //     {},
 //     {}
 // ]
+
+/***** 2 steps of Calling and using data via API******/
+// Calling a URL to get the data using 'fetch()' method in our code
+// Convert the incoming 'JSON type data' into 'Object type data'
+  
---
+++ 02_basics/tsFiles/04_objects.ts
@@ -0,0 +1,87 @@
+interface TinderUser {
+  id: string;
+  name: string;
+  isLoggedIn: boolean;
+}
+
+const tinderUser2: TinderUser = {
+  id: "123abc",
+  name: "Sammy",
+  isLoggedIn: false,
+};
+
+/***---------------------------------***/
+
+interface FullName {
+  userFullName: {
+    firstName: string;
+    lasName: string;
+  };
+}
+
+interface RegularUser {
+  email: string;
+  fullName: FullName;
+}
+
+const regularUser2: RegularUser = {
+  email: "some@gmail.com",
+  fullName: {
+    userFullName: {
+      firstName: "hitesh",
+      lasName: "Choudhary",
+    },
+  },
+};
+
+/***---------------------------------***/
+
+const obj4: Record<number, string> = { 1: "a", 2: "b" };
+const obj5: Record<number, string> = { 3: "c", 4: "d" };
+
+const obj6: Record<number, string> = { ...obj4, ...obj5 };
+const obj7: Record<number, string> = Object.assign({}, obj4, obj5);
+
+/***---------------------------------***/
+
+interface User {
+  id: number;
+  email: string;
+}
+
+const user2: User[] = [
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+  {
+    id: 2,
+    email: "i@gmail.com",
+  },
+  {
+    id: 3,
+    email: "j@gmail.com",
+  },
+];
+
+/***---------------------------------***/
+/*--De-structuring Object--*/
+interface Course {
+  courseName: string;
+  price: number;
+  courseInstructor: string;
+}
+
+const course2: Course = {
+  courseName: "js in hindi",
+  price: 999,
+  courseInstructor: "hitesh",
+};
+
+const { courseInstructor } = course2;
+//console.log(courseInstructor);
+
+/*--De-structuring Array--*/
+let myArray2 = [1, 2, 3];
+
+let [firstValue2, secondValue2, thirdValue2] = myArray;
